aabb31ff1dbb60d3b9a0be6bb9e19d76eb6c10ba
ANIKET GHATAGE <128148838+AniketGhatage123@users.noreply.github.com>
2023-03-28 07:56:05 -0700

    Add files via upload

---
+++ assignment 2.1/patt9.java
@@ -0,0 +1,16 @@
+class patt9 
+{
+	public static void main(String args[]){
+		char alpha=64;
+		for(int i=1;i<=5;i++){
+			for(int j=1; j<=5-i;j++){
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++){
+				System.out.print(" "+(char)(alpha+j));
+				
+			}
+			System.out.println();
+		}
+	}
+}
